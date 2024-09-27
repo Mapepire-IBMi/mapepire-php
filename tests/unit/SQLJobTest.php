3de7f3c50a7405119e84ed9c0370372224213171
jwoehr <jwoehr@softwoehr.com>
2024-09-26 23:31:25 -0600

    update test cases

--- tests/unit/SQLJobTest.php
+++ tests/unit/SQLJobTest.php
@@ -2,13 +2,16 @@
 
 use PHPUnit\Framework\TestCase;
 use Mapepire\SQLJob;
+use Mapepire\DaemonServer;
 
 class SQLJobTest extends TestCase
 {
 
     protected function setup(): void
     {
-        $this->SQLJob = SQLJob::SQLJobFromEnv(dir: 'tests/unit');
+        $this->sqlJob = SQLJob::SQLJobFromEnv(dir: 'tests/unit');
+        $this->ds = DaemonServer::DaemonServerFromDotEnv(dir: 'tests/unit');
+        $this->sqlJobDs = SQLJob::SQLJobFromDaemonServer(daemonServer: $this->ds);
     }
 
     public function testToString(): void
@@ -23,12 +26,31 @@ ignoreUnauthorized: 1
 Websocket\Client: WebSocket\Client(closed)
 "
         );
-        print $this->SQLJob;
+        print $this->sqlJob;
+    }
+
+    public function testToStringDs(): void
+    {
+        $this->expectOutputString(
+            expectedString:
+            "Mapepire\SQLJob
+host: localhost
+port: 8076
+user: meMyself
+ignoreUnauthorized: 1
+Websocket\Client: WebSocket\Client(closed)
+"
+        );
+        print $this->sqlJobDs;
     }
 
     protected function tearDown(): void
     {
-        unset($this->SQLJob);
+        unset($this->sqlJob);
+        unset($this->ds);
+        unset($this->sqlJobDs);
     }
-    private SQLJob $SQLJob;
+    private SQLJob $sqlJob;
+    private SQLJob $sqlJobDs;
+    private DaemonServer $ds;
 }
